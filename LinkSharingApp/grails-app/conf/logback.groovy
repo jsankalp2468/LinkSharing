fa46bb54906a6a72c6792d5c34fd71807b4246e7
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-28 13:52:00 +0530

    Finished Q10 : pattern changed in logback.groovy

--- LinkSharingApp/grails-app/conf/logback.groovy
+++ LinkSharingApp/grails-app/conf/logback.groovy
@@ -13,12 +13,14 @@ appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         charset = Charset.forName('UTF-8')
 
-        pattern =
-                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
-                        '%clr(%5p) ' + // Log level
-                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
-                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
-                        '%m%n%wex' // Message
+        pattern = "%d{MM/dd HH:mm:ss} %-5p %c{12} - %m%n"
+
+//        pattern =
+//                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+//                        '%clr(%5p) ' + // Log level
+//                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+//                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+//                        '%m%n%wex' // Message
     }
 }
 
