79235fc006521b168f61a6044618d13744367c7c
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-28 21:28:20 +0530

    Finished Q1 : User domain is created within user package with given 10 fields

---
+++ LinkSharingApp/grails-app/domain/User/User.groovy
@@ -0,0 +1,18 @@
+package User
+
+class User {
+
+    String firstName
+    String lastName
+    String email
+    String userName
+    String password
+    byte photo
+    boolean admin
+    boolean active
+    Date dateCreated
+    Date lastUpdated
+
+    static constraints = {
+    }
+}
