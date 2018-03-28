41d0dfbfebe9da7ee70953627c754fb6f2ba15d4
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-28 21:50:46 +0530

    Finished Q2 : Constraints are added in User.groovy Domain class excluding test cases

--- LinkSharingApp/grails-app/domain/User/User.groovy
+++ LinkSharingApp/grails-app/domain/User/User.groovy
@@ -14,5 +14,12 @@ class User {
     Date lastUpdated
 
     static constraints = {
+        email(email: true,nullable: false,blank: false,unique: true)//Nullable : Allows a property to be set to null - defaults to false.
+        password(nullable: false,blank: false,min: 5)
+        firstName(nullable: false,blank: false)
+        lastName(nullable: false,blank: false)
+        photo(nullable: true)
+        admin(nullable: true)
+        active(nullable: true)
     }
 }
