4875a9b873f9d3ded91da0cffa85263df721eed9
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-28 16:47:34 +0530

    Finished Q13 : app-config.yml is created and config properties are fetched from it. It is verified through boootstrap.grrovy.

--- LinkSharingApp/grails-app/init/linksharingapp/BootStrap.groovy
+++ LinkSharingApp/grails-app/init/linksharingapp/BootStrap.groovy
@@ -5,6 +5,8 @@ import grails.util.Holders
 class BootStrap {
 
     def init = { servletContext ->
+        log.info("**********************************************")
+        println(Holders.grailsApplication.config.server.contextPath)
     }
     def destroy = {
     }
