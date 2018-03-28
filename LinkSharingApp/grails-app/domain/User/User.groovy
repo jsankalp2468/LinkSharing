ea111ffd334a927bddbaef0a5a52b373f75cf445
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-28 22:53:41 +0530

    Finished Q2 : Test Cases Added

--- LinkSharingApp/grails-app/domain/User/User.groovy
+++ LinkSharingApp/grails-app/domain/User/User.groovy
@@ -8,16 +8,27 @@ class User {
     String userName
     String password
     byte photo
-    boolean admin
-    boolean active
+    Boolean admin
+    Boolean active
     Date dateCreated
     Date lastUpdated
 
+    User(String firstName, String lastName, String email, String userName, String password, byte photo, Boolean admin, Boolean active) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.email = email
+        this.userName = userName
+        this.password = password
+        this.photo = photo
+        this.admin = admin
+        this.active = active
+    }
     static constraints = {
         email(email: true,nullable: false,blank: false,unique: true)//Nullable : Allows a property to be set to null - defaults to false.
-        password(nullable: false,blank: false,min: 5)
+        password(nullable: false,blank: false,minSize: 5 )
         firstName(nullable: false,blank: false)
         lastName(nullable: false,blank: false)
+        userName(unique: true)
         photo(nullable: true)
         admin(nullable: true)
         active(nullable: true)
--- LinkSharingApp/src/test/groovy/User/UserSpec.groovy
+++ LinkSharingApp/src/test/groovy/User/UserSpec.groovy
@@ -15,4 +15,79 @@ class UserSpec extends Specification implements DomainUnitTest<User> {
         expect:"fix me"
             true == false
     }
+
+    void "validating user with every possible constraints"(){
+        setup:
+        User user = new User(firstName: fname,lastName: lname,email: email,userName: uname,password: pswd,photo: photo,admin: admin,active: active)
+        when:
+        Boolean result = user.validate()
+        then:
+        result == value
+
+        where:
+        fname       | lname     | email                         | uname     | pswd      | photo | admin | active | value
+        "sankalp"   |"jain"     |"sankalp.jain@tothenew.com"    |"jsankalp" | "abcdef"  | 1     | true  | true   |  true
+        //Email constraints
+        "sankalp"   |"jain"     |"sankalp.jain@to"              |"jsankalp" | "abcdef"  | 1     | true  | true   |  false
+        "sankalp"   |"jain"     |  null                         |"jsankalp" | "abcdef"  | 1     | true  | true   |  false
+        "sankalp"   |"jain"     |""                             |"jsankalp" | "abcdef"  | 1     | true  | true   |  false
+        //password constraints
+        "sankalp"   |"jain"     |"sankalp.jain@tothenew.com"    |"jsankalp" | null      | 1     | true  | true   |  false
+        "sankalp"   |"jain"     |"sankalp.jain@tothenew.com"    |"jsankalp" | "abcd"    | 1     | true  | true   |  false
+        "sankalp"   |"jain"     |"sankalp.jain@tothenew.com"    |"jsankalp" | ""        | 1     | true  | true   |  false
+        //firstname constraints
+        null        |"jain"     |"sankalp.jain@tothenew.com"    |"jsankalp" | "abcdef"  | 1     | true  | true   |  false
+        ""          |"jain"     |"sankalp.jain@tothenew.com"    |"jsankalp" | "abcdef"  | 1     | true  | true   |  false
+        //lastname constraints
+        "sankalp"   |null       |"sankalp.jain@tothenew.com"    |"jsankalp" | "abcdef"  | 1     | true  | true   |  false
+        "sankalp"   |""         |"sankalp.jain@tothenew.com"    |"jsankalp" | "abcdef"  | 1     | true  | true   |  false
+        //photo constraint
+        "sankalp"   |"jain"     |"sankalp.jain@tothenew.com"    |"jsankalp" | "abcdef"  | 0     | true  | true   |  true
+        //admin constraint
+        "sankalp"   |"jain"     |"sankalp.jain@tothenew.com"    |"jsankalp" | "abcdef"  | 0     | null  | true   |  true
+//        active constraint
+        "sankalp"   |"jain"     |"sankalp.jain@tothenew.com"    |"jsankalp" | "abcdef"  | 0     | true  | null   |  true
+    }
+
+    void "validating uniqueness of email"(){
+        setup:
+        String email = "sankalp.jain@tothenew.com"
+
+        when:
+        User user1 = new User(firstName: "sankalp",lastName: "jain",email: email,userName: "jsank",password: "abcdef",photo: 1,admin: true,active: null)
+        user1.save()
+
+        then:
+        user1.count() == 1
+
+        when:
+        User user2 = new User(firstName: "neelesh",lastName: "bansal",email: email,userName: "neelesh",password: "abcdef",photo: 1,admin: true,active: null)
+        user2.save()
+
+        then:
+        user2.count() == 1
+        user2.errors.allErrors.size() == 1
+        user2.errors.getFieldErrorCount('email') == 1
+    }
+
+    void "validating uniqueness of username"(){
+        setup:
+        String uname = "jsankalp"
+
+        when:
+        User user1 = new User(firstName: "sankalp",lastName: "jain",email: "sankalp.jain@ttn.com",userName: uname,password: "abcdef",photo: 1,admin: true,active: null)
+        user1.save()
+
+        then:
+        user1.count() == 1
+
+        when:
+        User user2 = new User(firstName: "neelesh",lastName: "bansal",email: "neelesh.bansal@ttn.com",userName: uname,password: "abcdef",photo: 1,admin: true,active: null)
+        user2.save()
+
+        then:
+        user2.count() == 1
+        user2.errors.allErrors.size() == 1
+        user2.errors.getFieldErrorCount('userName') == 1
+    }
 }
