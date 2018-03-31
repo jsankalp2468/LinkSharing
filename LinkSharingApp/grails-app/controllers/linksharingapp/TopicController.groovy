72b6d56d7b647ecfaf8f9a0118a28fe687110b39
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-31 14:19:26 +0530

    Finished Q16

--- LinkSharingApp/grails-app/controllers/linksharingapp/TopicController.groovy
+++ LinkSharingApp/grails-app/controllers/linksharingapp/TopicController.groovy
@@ -4,5 +4,11 @@ class TopicController {
 
     def index() { }
 
-    def show(Long id){ }
+    def show(Long id){
+        Topic topic = Topic.findById(id)
+        if (!topic) {
+            flash.error = "NO SUCH TOPIC"
+            redirect(controller: 'logIn', action: 'index')
+        }
+    }
 }
