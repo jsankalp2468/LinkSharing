4882cd1cd01dccd63479253a51d7d8eaf82bba13
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-31 20:12:32 +0530

    Finished Q7 : enum visibility now consists of method which checks if a string is valid enum or not .

--- LinkSharingApp/src/main/groovy/enumeration/Visibility.groovy
+++ LinkSharingApp/src/main/groovy/enumeration/Visibility.groovy
@@ -1,5 +1,17 @@
 package enumeration
 
 enum Visibility {
-    PUBLIC,PRIVATE
+    PUBLIC, PRIVATE
+
+    static Visibility isVisibility(String s) {
+        if (s.equalsIgnoreCase("PUBLIC")) {
+            return PUBLIC
+        } else if (s.equalsIgnoreCase("PRIVATE")) {
+            return PRIVATE
+        } else {
+            throw new Exception("Not a valid Visibility")
+        }
+    }
 }
+
+
