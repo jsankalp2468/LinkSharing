6cd3f0ea71a974c6d94cdc2f7e09f4edf2a45cbf
jsankalp2468 <sankalp.jain@tothenew.com>
2018-03-31 19:47:50 +0530

    Finished Q4 : Exception handeling of delete action at ResourceController.

--- LinkSharingApp/grails-app/controllers/linksharingapp/ResourceController.groovy
+++ LinkSharingApp/grails-app/controllers/linksharingapp/ResourceController.groovy
@@ -6,7 +6,15 @@ class ResourceController {
 
     def delete(Long id){
         Resource resource = Resource.load(id)
-        println(resource.delete(flush:true))//returns null
-        render("Resource deleted successfully")
+        //if println(resource.id) it throw error at resource.delete wali line
+        //but if we assign a value to it then it thrown exception at prsource.id=50
+        println(resource.id)
+        try{
+            println(resource.delete(flush:true))//returns null
+            render("Resource deleted successfully")
+        }
+        catch (RuntimeException ex){
+            render("Resource not found")
+        }
     }
 }
